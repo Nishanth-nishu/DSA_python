0f46dcdc219f7a53c52cd0ebb6bdad86369acbd1
R Nishanth <nishanth0962333@gmail.com>
2025-04-30 15:20:27 +0530

    neet code 1

---
+++ neet_code_Contains Duplicate_.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        check=[]
+        for i in range(0,len(List)):
+            if List[i] in check:
+                return True
+            else:
+                check.append(List[i])
+        return False
+
+         
